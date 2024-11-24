2fbcde453a27905619470be319f8e00723520a66
Soumitree Jena <soumitree.jena@anyhowinfo.com>
2024-11-24 13:50:00 +0530

    about us page code

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import AboutUs from './components/AboutUs/AboutUs'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='bg-red-500'>Hey I am here</h1>
+    <div className="min-h-screen bg-white-500 overflow-y-auto p-6">
+    <AboutUs/>
+    <h1 className='bg-red-500'>Hey I am here</h1>
+    </div>
     </>
   )
 }
